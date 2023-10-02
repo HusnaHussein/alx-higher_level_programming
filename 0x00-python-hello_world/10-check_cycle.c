13998c2d9c7b7b82a791d7d8ae085604a32139a1
HusnaHussein <husnahussainkilalo@gmail.com>
2023-10-02 06:17:34 -0700

    More tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	unsigned int n;
+	/* number of nodes */
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+	return (n);
+}
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
