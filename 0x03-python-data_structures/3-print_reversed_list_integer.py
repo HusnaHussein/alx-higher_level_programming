2c6478adcf106c3e2cde1800fceb0ac3d41c34fb
HusnaHussein <husnahussainkilalo@gmail.com>
2023-10-08 08:20:07 -0700

    Task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        new_list = my_listi
+        new_list.reverse()
+
+        for element in new_list:
+            print("{:d}".format(element))
